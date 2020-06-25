4771e9456fd4d3bc1398d1e76f7d03b24ddbaca5
Marcus <marcusnesk@gmail.com>
2020-06-26 00:24:46 +0200

    added files

---
+++ src/robots.js
@@ -0,0 +1,110 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Alyssa Gal',
+    username: 'cid.fiend',
+    email: 'alyssa.gal@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'Simon Ruud',
+    username: 'simonsr00',
+    email: 'simon.ruud@gmail.com'
+  },
+  {
+    id: 13,
+    name: 'Mathilde Neskvern',
+    username: 'Matnesk',
+    email: 'matnes@gmail.com'
+  },
+  {
+    id: 14,
+    name: 'Hans Neskvern',
+    username: 'Henesk',
+    email: 'heneskvern@gmail.com'
+  },
+  {
+    id: 15,
+    name: 'Line Neskvern',
+    username: 'Linenesk',
+    email: 'linenesk@gmail.com'
+  },
+  {
+    id: 16,
+    name: 'Marcus Neskvern',
+    username: 'marcusneskvern',
+    email: 'marcusnesk@gmail.com'
+  },
+  {
+    id: 17,
+    name: 'Andreas Lyngnes',
+    username: 'andrealy',
+    email: 'andrealy@hiof.no'
+  },
+  {
+    id: 18,
+    name: 'Georg Kaktus',
+    username: 'kaktus_gerg',
+    email: 'kaktgerg@gmail.no'
+  }
+];
